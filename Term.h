ec0c83dd4013420f616fe3d4e295ff64974eaac6
Danny Mitchell <dannymitchell@gmail.com>
2015-10-02 04:16:48 -0500

    Update Term.h
    I think this should complete the necessary declarations for the Term header. We should create a separate header file for the Polynomial, with the list of terms, constructor, destructor, another overloaded operator to add two polynomials.

--- Term.h
+++ Term.h
@@ -1,14 +1,31 @@
+#ifndef TERM_H
+#define TERM_H
+
 #include <iostream>
 
 using namespace std;
 
 class Term{
 private:
-int coefficient;
-int exponent;
+  int coefficient;
+  int exponent;
 public:
-Term();
-Term(int t_coeff, int t_exp);
-Term(Term& a_Term);
-int getexponent() const;
-bool operator >(Term& rhs);
+  Term();
+  Term(int t_coeff, int t_exp);
+  Term(Term& a_Term);
+  
+  int getcoefficient(void) const;
+  int getexponent(void) const;
+  void setcoefficient(int);
+  void setexponent(int);
+  
+  bool Term::operator<(const Term& rhs);
+  bool Term::operator>(const Term& rhs);
+  bool Term::operator==(const Term& rhs);
+
+  const Term& Term::operator=(const Term& rhs); 
+  const Term operator+(const Term& rhs) const; //used for adding two terms
+  
+  friend ostream& operator<<(ostream& output, const Term& aTerm);
+};
+#endif
